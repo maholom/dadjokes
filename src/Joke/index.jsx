5a4027de1c7a2392c5c554a3e4eb50836c8f7b73
MartaKirchgessner <martakirchgessner@gmail.com>
2020-11-21 11:57:31 +0100

    React vytvoreni komponenty Joke s exporty a importy

---
+++ src/Joke/index.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import './style.css';
+
+
+export const Joke = (props) => {
+
+  const [pocetLike, setPocetLike] = useState(0)
+  const [pocetDislike, setPocetDislike] = useState(0)
+
+  return (
+    <>
+    <div className="container">
+      <div className="joke">
+        <div className="joke__body">
+          <div className="joke__user">
+            <img className="user-avatar" src="assets/img/user01.png" />
+            <p className="user-name">Neroxx</p>
+          </div>
+
+          <p className="joke__text">
+            The secret service isn't allowed to yell "Get down!" anymore when
+            the president is about to be attacked. Now they have to yell
+            "Donald, duck!"
+          </p>
+        </div>
+
+        <div className="joke__likes">
+
+          <button onClick={() => {
+        setPocetLike(pocetLike + 1);}} id="btn-up" className="btn-like btn-like--up"></button>
+          <span id="likes-up" className="likes-count likes-count--up">{pocetLike}</span>
+
+          <button onClick={() => {
+        setPocetDislike(pocetDislike + 1);}}
+          id="btn-down" className="btn-like btn-like--down"></button>
+          <span id="likes-down" className="likes-count likes-count--down">{pocetDislike}</span>
+        </div>
+      </div>
+    </div>
+</>
+  )
+}
--- src/index.jsx
+++ src/index.jsx
@@ -1,45 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { render } from 'react-dom';
+import { Joke } from './Joke'
 import './index.html';
-import './style.css';
+
 
 const App = () => {
-  const [pocetLike, setPocetLike] = useState(0)
-  const [pocetDislike, setPocetDislike] = useState(0)
-
-  return (
-  <>
-    <div className="container">
-      <div className="joke">
-        <div className="joke__body">
-          <div className="joke__user">
-            <img className="user-avatar" src="assets/img/user01.png" />
-            <p className="user-name">Neroxx</p>
-          </div>
-
-          <p className="joke__text">
-            The secret service isn't allowed to yell "Get down!" anymore when
-            the president is about to be attacked. Now they have to yell
-            "Donald, duck!"
-          </p>
-        </div>
-
-        <div className="joke__likes">
-
-          <button onClick={() => {
-        setPocetLike(pocetLike + 1);}} id="btn-up" className="btn-like btn-like--up"></button>
-          <span id="likes-up" className="likes-count likes-count--up">{pocetLike}</span>
-
-          <button onClick={() => {
-        setPocetDislike(pocetDislike + 1);}}
-          id="btn-down" className="btn-like btn-like--down"></button>
-          <span id="likes-down" className="likes-count likes-count--down">{pocetDislike}</span>
-        </div>
-      </div>
-    </div>
-</>
-);
-}
+    return (
+  <Joke />
+)};
 
 
 render (< App />, document.querySelector('#app'))
